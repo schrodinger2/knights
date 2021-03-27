86a795392d7a936e705dd3e4cf5e97c270420447
schrodinger2 <schrodinger2@users.noreply.github.com>
2021-03-27 11:42:31 +0300

    opserving where the knight spawns

--- knights.js
+++ knights.js
@@ -5,12 +5,27 @@ document.body.onload =  () => {
     for (var j = 0; j < 8; j++) {
       var newDiv = document.createElement("div");
       newDiv.className = "square";
+
+
       if ( (i+j) % 2 == 1) {
         newDiv.classList.add("black");
       }else {
         newDiv.classList.add("white");
       }
       document.body.querySelector(".board").appendChild(newDiv)
+
+
+      newDiv.classList.add(`j${j}i${i}`); //to ideintify the squares
+      // let square = document.body.querySelectorAll(".square")[i*8 + j];
+      let square = document.body.querySelector(`.j${j}i${i}`);
+      if (square) {
+        square.addEventListener('click', () => {
+          let knight = document.createElement("div");
+          knight.className = "knight";
+          square.appendChild(knight)
+
+        });
+        }
     }
   }
 
