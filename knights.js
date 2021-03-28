77bb44db4330f0e4ff83fbfbc3011804b715df7a
schrodinger2 <schrodinger2@users.noreply.github.com>
2021-03-28 09:05:26 +0300

    moving the knights

--- knights.js
+++ knights.js
@@ -1,3 +1,4 @@
+let g = 1;
 document.body.onload =  () => {
   let board = document.querySelector(".board");
   for (var i = 0; i < 8; i++) {
@@ -16,17 +17,75 @@ document.body.onload =  () => {
 
 
       newDiv.classList.add(`j${j}i${i}`); //to ideintify the squares
-      // let square = document.body.querySelectorAll(".square")[i*8 + j];
+
+
       let square = document.body.querySelector(`.j${j}i${i}`);
-      if (square) {
-        square.addEventListener('click', () => {
+
+      square.addEventListener('click', () => {
           let knight = document.createElement("div");
           knight.className = "knight";
           square.appendChild(knight)
-
-        });
-        }
+      });
     }
   }
+}
 
+setInterval( () => { movingKnights()}, 1000);
+
+function movingKnights() {
+  let knights = document.body.querySelectorAll(".knight")
+
+  for (var i = 0; i < knights.length; i++) {
+    let squareID = knights[i].parentNode.classList[2];
+    //the idintifier class has the position of the square (y and i)
+
+    let jth = squareID[1]; //j of the current parentNode
+    let ith = squareID[3]; //i of the current parentNode
+
+    let newJAndI = randomMoves(jth, ith);
+    let newParentNode = document.body.querySelector(`.j${newJAndI[0]}i${newJAndI[1]}`);
+    newParentNode.appendChild(knights[i])
+  }
+}
+
+
+
+function randomMoves(j, i) {
+  // The knight moves "L" shape in any direction
+  // So plus or minus 2 in the X axis and plus or minus 1 in the Y axis
+  // Or plus or minus 1 in the X axis and plus or minus 2 in the Y axis
+
+  let moveOneStepAt;
+  let moveTwoStepsAt;
+  if (Math.floor(Math.random()*2) == 0) {
+    moveTwoStepsAt = parseInt(j);
+    moveOneStepAt = parseInt(i);
+  }else {
+    moveTwoStepsAt = parseInt(i);
+    moveOneStepAt = parseInt(j);
+  }
+
+
+  if (Math.floor(Math.random()*2) == 0) {
+    moveTwoStepsAt += 2
+  }else {
+    moveTwoStepsAt -= 2
+  }
+
+
+  if (Math.floor(Math.random()*2) == 0) {
+    moveOneStepAt += 1;
+  }else {
+    moveOneStepAt -= 1;
+  }
+
+  // getting the new values of i and j
+  if (twoStepsAtIOrJ = 0) {
+    j = moveTwoStepsAt
+    i = moveOneStepAt
+  }else {
+    i = moveTwoStepsAt
+    j = moveOneStepAt
+  }
+  return [j, i];
 }
