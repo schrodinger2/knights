906c0f6d56816446ce6961473419969bbeae2cec
schrodinger2 <schrodinger2@users.noreply.github.com>
2021-03-29 09:48:03 +0300

    png pic of the knight

--- knights.js
+++ knights.js
@@ -1,36 +1,61 @@
 let boardLength = 8;
 document.body.onload =  () => {
-  let board = document.querySelector(".board");
+
+  //making the squares
   for (var i = 0; i < boardLength; i++) {
     // there are 64 squares in a chess board
     for (var j = 0; j < boardLength; j++) {
-      var newDiv = document.createElement("div");
-      newDiv.className = "square";
+      let newSquare = document.createElement("div");
+      newSquare.className = "square";
 
-
+      //styling the square one black and one white
       if ( (i+j) % 2 == 1) {
-        newDiv.classList.add("black");
+        newSquare.classList.add("black");
       }else {
-        newDiv.classList.add("white");
+        newSquare.classList.add("white");
       }
-      document.body.querySelector(".board").appendChild(newDiv)
+      document.body.querySelector(".board").appendChild(newSquare)
 
 
-      newDiv.classList.add(`j${j}i${i}`); //to ideintify the squares
+      newSquare.classList.add(`j${j}i${i}`); //to ideintify the squares
 
 
-      let square = document.body.querySelector(`.j${j}i${i}`);
+      newSquare.addEventListener('click', () => {
+          // let knight = document.createElementNS("http://www.w3.org/2000/svg","svg");
+          //
+          // knight.setAttribute('version', "1.1");
+          // knight.setAttribute('id', `layer${j}${i}`);
+          // knight.setAttribute('xmlns', "http://www.w3.org/2000/svg");
+          // knight.setAttribute('xmlns:xlink', "http://www.w3.org/1999/xlink");
+          // knight.setAttribute('x', "0px");
+          // knight.setAttribute('y', "0px");
+          // knight.setAttribute('viewBox', "0 0 50 50");
+          // knight.setAttribute('style', "enable-background:new 0 0 50 50;");
+          // knight.setAttribute('xml:space', "preserve");
+          // let path1 = document.createElement("path");
+          // path1.setAttribute('d', "M40,44.5H12v-0.54c0-0.81,0.65-1.46,1.46-1.46h25.08c0.81,0,1.46,0.65,1.46,1.46V44.5z");
+          // knight.appendChild(path1);
+          // let path2 = document.createElement("path");
+          // path2.setAttribute('d', "M38.5,41.5h-25c0-0.55,0.22-1.05,0.59-1.41c0.36-0.37,0.86-0.59,1.41-0.59h21C37.6,39.5,38.5,40.4,38.5,41.5z");
+          // knight.appendChild(path2);
+          // let path3 = document.createElement("path");
+          // path3.setAttribute('d', "M34.5,28c0,3.25-0.31,6.42-0.91,9.5H19.63c1.21-2.93,1.87-6.14,1.87-9.5c0-2.31-0.31-4.55-0.91-6.68C20.56,21.56,19.92,25.94,17,24c-3-2-9,3-9,3l-1-2c0,0,1-5,10.08-11.19C26.16,7.62,28.4,4.06,28.4,4.06C32.29,11.17,34.5,19.33,34.5,28z");
+          // knight.appendChild(path3);
 
-      square.addEventListener('click', () => {
-          let knight = document.createElement("div");
-          knight.className = "knight";
-          square.appendChild(knight)
+           let knight = document.createElement("img");
+           knight.src = "./knight.png"
+
+           // let knight = document.createElement("div");
+           knight.className = "knight";
+          newSquare.appendChild(knight)
       });
     }
   }
 }
 
 setInterval( () => { movingKnights()}, 1000);
+
+
 
 function movingKnights() {
   let knights = document.body.querySelectorAll(".knight")
